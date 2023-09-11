237b5f886e20a69f4b6de699b24acc7d1be021f1
youHappyOK <luojin_mail@163.com>
2023-09-11 21:27:23 +0800

    1

---
+++ test_ocr.py
@@ -0,0 +1,29 @@
+from paddleocr import PaddleOCR, draw_ocr
+
+# Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
+# 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
+# ocr = PaddleOCR(use_angle_cls=True, lang="ch", page_num=2)
+
+# det_model_dir 检测模型，用于确定文本方位
+# rec_model_dir 识别模型，用于识别文本内容
+# cls_model_dir  文本方向分类模型，支持0和180度文本
+# use_angle_cls
+ocr = PaddleOCR(det_model_dir='./det_model', rec_model_dir='./rec_model',
+                cls_model_dir='./cls_model', use_angle_cls=True)
+img_path = './chat.png'
+result = ocr.ocr(img_path, cls=True)
+for idx in range(len(result)):
+    res = result[idx]
+    for line in res:
+        print(line)
+
+# 显示结果
+from PIL import Image
+result = result[0]
+image = Image.open(img_path).convert('RGB')
+boxes = [line[0] for line in result]
+txts = [line[1][0] for line in result]
+scores = [line[1][1] for line in result]
+im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/simfang.ttf')
+im_show = Image.fromarray(im_show)
+im_show.save('result.jpg')
---
+++ test_ocr_visual_pdf.py
@@ -0,0 +1,47 @@
+from paddleocr import PaddleOCR, draw_ocr
+
+# Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
+# 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
+# need to run only once to download and load model into memory，未指定模型会默认下载v4轻量模型
+# ocr = PaddleOCR(use_angle_cls=True, lang="ch", page_num=2)
+
+# det_model_dir 检测模型，用于确定文本方位
+# rec_model_dir 识别模型，用于识别文本内容
+# cls_model_dir  文本方向分类模型，支持0和180度文本
+# use_angle_cls
+ocr = PaddleOCR(det_model_dir='./det_model', rec_model_dir='./rec_model',
+                cls_model_dir='./cls_model', use_angle_cls=True)
+img_path = r'./chat.png'
+result = ocr.ocr(img_path, cls=True)
+for idx in range(len(result)):
+    res = result[idx]
+    for line in res:
+        print(line)
+
+# 显示结果
+import fitz
+from PIL import Image
+import cv2
+import numpy as np
+imgs = []
+with fitz.open(img_path) as pdf:
+    for pg in range(0, pdf.pageCount):
+        page = pdf[pg]
+        mat = fitz.Matrix(2, 2)
+        pm = page.getPixmap(matrix=mat, alpha=False)
+        # if width or height > 2000 pixels, don't enlarge the image
+        if pm.width > 2000 or pm.height > 2000:
+            pm = page.getPixmap(matrix=fitz.Matrix(1, 1), alpha=False)
+
+        img = Image.frombytes("RGB", [pm.width, pm.height], pm.samples)
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        imgs.append(img)
+for idx in range(len(result)):
+    res = result[idx]
+    image = imgs[idx]
+    boxes = [line[0] for line in res]
+    txts = [line[1][0] for line in res]
+    scores = [line[1][1] for line in res]
+    im_show = draw_ocr(image, boxes, txts, scores, font_path='doc/fonts/simfang.ttf')
+    im_show = Image.fromarray(im_show)
+    im_show.save('result_page_{}.jpg'.format(idx))
